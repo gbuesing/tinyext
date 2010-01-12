040873c6786ef95352fc65b3399245fb543b97d0
Geoff Buesing <geoff@lifesnapz.com>
2010-01-12 16:59:11 -0600

    Move core extensions into core. Add net/http monkeypatch

--- lib/tinyext.rb
+++ lib/tinyext.rb
@@ -1,4 +1,1 @@
-require 'tinyext/blank'
-require 'tinyext/symbol'
-require 'tinyext/tap'
-require 'tinyext/try'
+require 'tinyext/core'
---
+++ lib/tinyext/core.rb
@@ -0,0 +1,4 @@
+require 'tinyext/core/blank'
+require 'tinyext/core/symbol'
+require 'tinyext/core/tap'
+require 'tinyext/core/try'
